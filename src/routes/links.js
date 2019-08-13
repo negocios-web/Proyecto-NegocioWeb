2d6ab2272f77556369d3f7c48426586897da2d73
luisdario71196 <luisdario500@gmail.com>
2019-08-13 02:22:12 -0600

    Listar kas clases matriculadas del alumno (arreglar)

--- src/routes/links.js
+++ src/routes/links.js
@@ -240,9 +240,20 @@ router.get('/verListaClaseAlumnosMatriculadas/:idOfertaClase', async (req, res) 
 
 });
 
+// Mostrar los pagos hechos por los alumnos
 router.get('/pagos', async (req, res) => {
 
     const links = await pool.query('SELECT tabla_detalle_matricula_alumno.`numeroCuenta`, tabla_alumnos.nombreCompleto, COUNT(`id_clase`) as clasesMatriculadas, CASE WHEN(SELECT COUNT(*) FROM tabla_pagos WHERE tabla_pagos.estado) = 1  THEN "PGD"  END as estado FROM`tabla_detalle_matricula_alumno` INNER JOIN tabla_pagos ON tabla_pagos.numeroCuenta = tabla_detalle_matricula_alumno.numeroCuenta INNER JOIN tabla_alumnos ON tabla_alumnos.numeroCuenta = tabla_detalle_matricula_alumno.numeroCuenta INNER JOIN tabla_oferta_clase ON tabla_oferta_clase.idOfertaClase = tabla_detalle_matricula_alumno.idOfertaClase INNER JOIN tabla_clases ON tabla_clases.id_clase = tabla_oferta_clase.idClase WHERE tabla_detalle_matricula_alumno.idOfertaClase GROUP BY numeroCuenta');
     res.render('links/pagos', { links });
+});
+
+// Ver lista de clases matriculadas por el almno
+router.get('/verListaClasesAlumno/:numeroCuenta', async (req, res) => {
+    const { numeroCuenta } = req.params;
+    const links = await pool.query('SELECT tabla_clases.id_clase, tabla_clases.nombreClase, tabla_detalle_matricula_alumno.`numeroCuenta`, tabla_alumnos.nombreCompleto FROM`tabla_detalle_matricula_alumno` INNER JOIN tabla_alumnos ON tabla_alumnos.numeroCuenta = tabla_detalle_matricula_alumno.numeroCuenta INNER JOIN tabla_oferta_clase ON tabla_oferta_clase.idOfertaClase = tabla_detalle_matricula_alumno.idOfertaClase INNER JOIN tabla_clases ON tabla_clases.id_clase = tabla_oferta_clase.idClase WHERE tabla_detalle_matricula_alumno.numeroCuenta = ?', [numeroCuenta]);
+    res.render('links/listClasesAlumnos', { links });
+    
+    console.log(links);
+    
 });
 module.exports = router;
